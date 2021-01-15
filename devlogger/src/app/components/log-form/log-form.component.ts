520c34bc6ac494002429886ca76e0607df6e0297
Kevin O'Keeffe <kevokeeffe@gmail.com>
2021-01-15 17:38:54 +0000

    Added CRUD functionality

--- devlogger/src/app/components/log-form/log-form.component.ts
+++ devlogger/src/app/components/log-form/log-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Log } from 'src/app/models/Log';
+import { LogService } from '../../services/log.service';
 
 @Component({
   selector: 'app-log-form',
@@ -6,10 +8,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-form.component.css']
 })
 export class LogFormComponent implements OnInit {
+  id:string;
+  text:string;
+  date:string;
+  isNew: boolean = true;
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit(): void {
+    // Subscribe to the selected log observable
+    this.logService.selectedLog.subscribe(log => {
+      console.log(log)
+      if(log.id !==null){
+        this.isNew = false;
+          this.id = log.id;
+          this.date = log.date;
+          this.text = log.text;
+      }
+    })
+  }
+
+  onSubmit(){
+    // check if new log
+    if(this.isNew){
+      //Create a new log
+      const newLog = {
+        id:this.generateId(),
+        text:this.text,
+        date:new Date()
+      }
+      this.logService.addLog(newLog)
+    }else{
+      // Create log to be updated
+      const updLog = {
+        id:this.id,
+        text:this.text,
+        date: new Date()
+      }
+      // Update log
+      this.logService.updateLog(updLog);
+    }
+    // Clear state
+    this.clearState()
+  }
+
+  clearState(){
+    this.isNew = true;
+    this.id = '';
+    this.date = '';
+    this.text = '';
+    this.logService.clearState();
+  }
+
+  generateId(){
+
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
   }
 
 }
---
+++ devlogger/src/app/services/log.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { Log } from '../models/Log';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LogService {
+  logs: Log[];
+  private logSource = new BehaviorSubject<Log>({
+    id: null,
+    text: null,
+    date: null,
+  });
+  selectedLog = this.logSource.asObservable();
+
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
+  constructor() {
+    // this.logs = [
+    //   {
+    //     id: '1',
+    //     text: 'generated component',
+    //     date: new Date('12/26/2017 10:54:23'),
+    //   },
+    //   {
+    //     id: '2',
+    //     text: 'added component',
+    //     date: new Date('12/26/2017 11:54:23'),
+    //   },
+    //   {
+    //     id: '3',
+    //     text: 'updated component',
+    //     date: new Date('12/26/2017 12:44:23'),
+    //   },
+    // ];
+    this.logs=[];
+  }
+
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+  }
+
+  deleteLog(log:Log){
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+  }
+
+  clearState(){
+    this.stateSource.next(true);
+  }
+}
